major changes since last commit. all of the r, s, and w updates are now functionalized in 'basis.py'. furthermore, the discretization for R(x) was incorrect for second order derivatives. had to make */dx^^2 not, */2dx. also have included conductivity feedback now and are getting closer to getting the right basis functions. W(k) is still off and is going to receive the most attention moving forward. as of this commit, the .tex writeup is very out of date and needs to be updated with corrections.

1DHeatEqnPGD.py
@@ -1,8 +1,10 @@
 import sys,os
 import os.path
 import numpy as np
+# from scipy import linalg
 import math
 import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
 import basis
 
 try:
@@ -16,24 +18,29 @@ except ImportError:
     StyDim='';
 
 ## Initializing Info
-k = 1 # W/(m*K) -> Bulk U metal at RT
+k = np.linspace(1,5,101-1) # W/(m*K)
 f = 1 # constant source term
 
-X = np.array([np.linspace(0,1,61)],ndmin=2) # spatial domain -> calculated values for R for first iteration are independent of this first initial guess. tested with parabolic and linear inital shapes and same X_i+1 result was obtained.
-T = np.array([np.linspace(0,0.1,151)],ndmin=2) # time domain
-K = np.array([np.linspace(1,5,101)],ndmin=2) # diffusivity domain
+X = np.array([np.zeros(61)],ndmin=2) # spatial domain -> calculated values for R for first iteration are independent of this first initial guess. tested with parabolic and linear inital shapes and same X_i+1 result was obtained.
+T = np.array([np.zeros(151)],ndmin=2) # time domain
+K = np.array([np.zeros(101)],ndmin=2) # diffusivity domain
 dx = 1/61
 dt = 0.1/151
 dk = 5/101
 
 ## guesses for R,S,W
 R_old = np.ones(np.shape(X)[1])
+R_old[0] = 0; R_old[-1] = 0
+
 S_old = np.ones(np.shape(T)[1])
+S_old[0] = 0; S_old[-1] = 0
+
 W_old = np.ones(np.shape(K)[1])
+W_new = np.ones(np.shape(K)[1])
 
 ## convergence criteria
-eps = 1E-4 # num of enrichment steps
-nu = 1E-4 # within enrichemnt
+eps = 1E-4 # num of enrichment steps - 2-norm
+nu = 1E-4 # within enrichemnt - inf-norm
 
 NumEnr=0 # number of enrichment steps
 
@@ -43,33 +50,44 @@ while nu_check >= nu:
     #######################
     ##   Solve for R(x)  ##
     #######################
-
+    print('\nSolving for R(x)')
     s1,s2,s3,s4,s5 = basis.updateS(S_old,T,dt)
+    # print('updatedS -> '+str(s1)+', '+str(s2)+', '+str(s3)+', '+str(s4)+', '+str(s5))
     w1,w2,w3,w4,w5 = basis.updateW(W_old,K,dk,k)
-
-    v1 = -(s1*w2/(2*dx))
-    v2 = (s1*w2)/dx + s2*w1
+    # print('updatedW -> '+str(w1)+', '+str(w2)+', '+str(w3)+', '+str(w4)+', '+str(w5))
+    v1 = -(s1*w2/(math.pow(dx,2)))
+    v2 = s2*w1 + (2*s1*w2)/math.pow(dx,2)
     v3 = v1
     b = 0.0
     for Elem in X:
+        # print('R(x) '+str(Elem[0]),', '+str(Elem[-1]))
         for cnt,idx in enumerate(Elem):
             if ((cnt==0) or (cnt==np.shape(X)[1]-1)): #boundary points
-                pass
+                b += 0
             else:
-                b += -Elem[cnt-1]*(s5*w5)/(2*dx) + Elem[cnt]*((s5*w5/dx)+s4*w4) - Elem[cnt+1]*(s5*w5)/(2*dx)
+                b += -Elem[cnt]*s4*w4 + ((Elem[cnt+1]-2*Elem[cnt]+Elem[cnt-1])/math.pow(dx,2))*s5*w5
     b += f*s3*w3
 
     R_Lmatrix = v1*np.eye(np.shape(X)[1], k=-1) + v2*np.eye(np.shape(X)[1], k=0) + v3*np.eye(np.shape(X)[1], k=1)
+    R_Lmatrix[0,:] = [0.0]
+    R_Lmatrix[0,0] = 1.0
+    R_Lmatrix[-1,:] = [0.0]
+    R_Lmatrix[-1,-1] = 1.0
     bVec = np.array((b*np.ones(np.shape(X)[1])))
-    bVec[0] = bVec[0]-v1
-    bVec[-1] = bVec[-1]-v3
+    bVec[0] = 0.0
+    bVec[-1] = 0.0
+    # bVec[0] = bVec[0]-v1
+    # bVec[-1] = bVec[-1]-v3
 
     R_new = np.dot(np.linalg.inv(R_Lmatrix),bVec)
+    # R_new = linalg.solve_triangular(R_Lmatrix,bVec)
 
     #######################
     ##   Solve for S(t)  ##
     #######################
+    print('Solving for S(t)')
     r1,r2,r3,r4,r5 = basis.updateR(R_new,X,dx)
+    # print('updatedR -> '+str(r1)+', '+str(r2)+', '+str(r3)+', '+str(r4)+', '+str(r5))
     # All 'w' integrals already solved for in R(x) solve. Don't need to resolve them.
 
     u1 = -(r1*w1)/(2*dt)
@@ -77,53 +95,74 @@ while nu_check >= nu:
     u3 = -u1
     y = 0.0
     for Elem in T:
+        # print('S(t) '+str(Elem[0]),', '+str(Elem[-1]))
         for cnt,idx in enumerate(Elem):
             if ((cnt==0) or (cnt==np.shape(T)[1]-1)): #boundary points
-                pass
+                y += 0
             else:
-                y += -Elem[cnt-1]*r5*w4/(2*dt) - Elem[cnt]*w5*r4 + Elem[cnt+1]*r5*w4/(2*dt)
+                y += -((Elem[cnt+1]-Elem[cnt-1])/(2*dt))*r5*w4 + Elem[cnt]*w5*r4
     y += f*r3*w3
 
     S_Lmatrix = u1*np.eye(np.shape(T)[1], k=-1) + u2*np.eye(np.shape(T)[1], k=0) + u3*np.eye(np.shape(T)[1], k=1)
+    S_Lmatrix[0,:] = [0.0]
+    S_Lmatrix[0,0] = 1.0
+    S_Lmatrix[-1,:] = [0.0]
+    S_Lmatrix[-1,-1] = 1.0
     yVec = np.array((y*np.ones(np.shape(T)[1])))
-    yVec[0] = yVec[0] - u1
-    yVec[-1] = yVec[-1] - u3
+    yVec[0] = 0.0
+    yVec[-1] = 0.0
+    # yVec[0] = yVec[0] - u1
+    # yVec[-1] = yVec[-1] - u3
 
     S_new = np.dot(np.linalg.inv(S_Lmatrix),yVec)
+    # S_new = linalg.solve_triangular(S_Lmatrix,yVec)
 
     #######################
     ##   Solve for W(k)  ##
     #######################
+    print('Solving for W(k)')
     r1,r2,r3,r4,r5 = basis.updateR(R_new,X,dx)
+    # print('updatedR -> '+str(r1)+', '+str(r2)+', '+str(r3)+', '+str(r4)+', '+str(r5))
     s1,s2,s3,s4,s5 = basis.updateS(S_new,T,dt)
+    # print('updatedS -> '+str(s1)+', '+str(s2)+', '+str(s3)+', '+str(s4)+', '+str(s5))
 
-    z = 0.0
-    for Elem in K:
-        for cnt,idx in enumerate(Elem):
-            if ((cnt==0) or (cnt==np.shape(T)[1]-1)): #boundary points
-                pass
-            else:
-                z += -(Elem[cnt]*r5*s4 - k*Elem[cnt]*r4*s5)
-    z += f*r3*s3
-    z = z/(r1*s2-k*s1*r2)
+    for idk,param in enumerate(k): # each element in conductivity range
+        tmp = 0.0
+        for enr in K: # each enrichment step
+            for test,value in enumerate(enr):
+                tmp += -(value*r5*s4 - param*value*r4*s5)
+                # print(test,tmp)
+        tmp += f*r3*s3
+        tmp = tmp/(r1*s2-param*s1*r2)
+        W_new[idk] = tmp
 
-    W_Lmatrix = np.eye(np.shape(K)[1], k=0)
-    zVec = np.array((z*np.ones(np.shape(K)[1])))
-
-    W_new = np.dot(np.linalg.inv(W_Lmatrix),zVec)
 
     #######################################
     ## Check for enrichment convergence  ##
     #######################################
-    # new = np.outer(np.outer(R_new,S_new),W_new)
-    new = np.tensordot(np.tensordot(R_new,S_new,axes=0),W_new,axes=0)
-    # print(np.shape(new))
-    old = np.tensordot(np.tensordot(R_old,S_old,axes=0),W_old,axes=0)
-    # print(np.shape(old))
 
+    # new = np.outer(np.outer(R_new,S_new),W_new)
+    # new = np.tensordot(np.tensordot(R_new,S_new,axes=0),W_new,axes=0)
+    # print(np.shape(new))
+    # old = np.tensordot(np.tensordot(R_old,S_old,axes=0),W_old,axes=0)
+    # print(np.shape(old))
     # nu_check = np.linalg.norm((new-old),np.inf)/np.linalg.norm(old,np.inf)
-    nu_check = np.amax(new-old)/np.amax(old)
-    print(str(nu_check))
+    # nu_check = abs(np.amax(new-old))/abs(np.amax(old))
+    # print(abs(np.amax(new-old)))
+    # print(abs(np.amax(old)))
+
+    nu_check = 0.0
+    for idr in np.arange(0,len(R_new)):
+        for ids in np.arange(0,len(S_new)):
+            for idw in np.arange(0,len(W_new)):
+                tmp = abs((R_new[idr]*S_new[ids]*W_new[idw] - R_old[idr]*S_old[ids]*W_old[idw]))
+                if tmp > nu_check:
+                    nu_check = tmp
+                else:
+                    pass
+
+    print(Cyan+str(nu_check))
+    # sys.exit()
     if nu_check >= nu:
         R_old = R_new
         S_old = S_new
@@ -137,19 +176,12 @@ while nu_check >= nu:
         print(Green+'\nEnrichment Step '+str(NumEnr)+' completed in '+str(enrCnt)+' steps.\n')
         break
 
-# ### epscheck should be reworked into the 2-norm ###
-# epscheck = max(np.outer(tmp,W))/max(np.outer(tmp,K)) # is inf-norm
-# if epscheck >= eps:
-# else:
-#     # enrichement process is done!
-#     break
-
 plt.figure(1)
 for cnt,elem in enumerate(X):
     if cnt == 0:
         pass
     else:
-        plt.plot(np.linspace(0,1,61), elem/max(elem), label = str(cnt), linewidth = 3)
+        plt.plot(np.linspace(0,1,61), elem/max(abs(elem)), label = str(cnt), linewidth = 3)
 plt.grid(True)
 plt.legend(loc='best',fontsize='x-small')
 
@@ -158,7 +190,7 @@ for cnt,elem in enumerate(T):
     if cnt == 0:
         pass
     else:
-        plt.plot(np.linspace(0,1,151), elem/max(elem), label = str(cnt), linewidth = 3)
+        plt.plot(np.linspace(0,0.1,151), elem/max(abs(elem)), label = str(cnt), linewidth = 3)
 plt.grid(True)
 plt.legend(loc='best',fontsize='x-small')
 
@@ -167,7 +199,15 @@ for cnt,elem in enumerate(K):
     if cnt == 0:
         pass
     else:
-        plt.plot(np.linspace(0,1,101), elem/max(elem), label = str(cnt), linewidth = 3)
+        plt.plot(np.linspace(1,5,101), elem/max(abs(elem)), label = str(cnt), linewidth = 3)
 plt.grid(True)
 plt.legend(loc='best',fontsize='x-small')
 plt.show()
+#
+# phi=0.0
+# for i,t,k in zip(X,T,K):
+#     phi += np.tensordot(np.tensordot(i,t,axes=0),k,axes=0)
+# print(np.shape(phi))
+# plt.figure(4)
+# Axes3D.plot_surface(phi[]0)
+# plt.show()

basis.py
@@ -4,59 +4,133 @@ import math
 def updateR(R,X,dx):
     r1=0; r2=0; r3=0; r4=0; r5=0
     for idr,dum in enumerate(R):
-        if ((idr==0) or (idr==len(R)-1)): # boundary points
+        if idr == len(R)-1: #far right boundary node
             pass
-        else: # interior points
-            r1 += dx/2 * (math.pow(R[idr],2)+math.pow(R[idr-1],2))
-            if idr == 1:
-                r2 += math.pow(dx,3)/8 * (R[idr]*(R[idr]-2*R[idr+1]+R[idr]) + R[idr-1]*(R[idr-1]-2*R[idr]+R[idr+1]))
+        else:
+            r1 += dx/2*(math.pow(R[idr+1],2) + math.pow(R[idr],2)) # O(h^2)
+            r3 += dx/2*(R[idr+1] + R[idr]) # O(h^2)
+            if idr == 0: # first node on LHS
+                r2 += 1/(2*dx)*( R[idr+1]*(R[idr+2]-2*R[idr+1]+R[idr]) + 0) # O(h^2)
+                # r2 += 1/(2*dx)*( R[idr+1]*(R[idr+2]-2*R[idr+1]+R[idr]) + R[idr]/4*(R[idr]-2*R[idr+1]+R[idr+2]) ) # O(h)
+            elif idr == len(R)-2: # next to last node on right side
+                r2 += 1/(2*dx)*( 0 + R[idr]*(R[idr+1]-2*R[idr]+R[idr-1]) ) # O(h^2)
+                # r2 += 1/(2*dx)*( R[idr+1]*(R[idr-1]-2*R[idr]+R[idr+1]) + R[idr]*(R[idr+1]-2*R[idr]+R[idr-1]) ) # O(h)
             else:
-                r2 += 1/4 * (R[idr]*(R[idr+1]-2*R[idr]+R[idr-1]) + R[idr-1]*(R[idr]-2*R[idr-1]+R[idr-2]))
-            r3 += dx/2 * (R[idr]+R[idr-1])
+                r2 += 1/(2*dx)*( R[idr+1]*(R[idr+2]-2*R[idr+1]+R[idr]) + R[idr]*(R[idr+1]-2*R[idr]+R[idr-1]) ) # O(h^2)
     for Elem in X:
         for cnt,pair in enumerate(zip(Elem,R)):
-            if ((cnt==0) or (cnt==np.shape(X)[1]-1)): #boundary points
+            if cnt == len(R)-1: # far right boundary cell
                 pass
-            else: #interior points
-                if cnt ==1:
-                    r4 += math.pow(dx,3)/8 * (R[cnt]*(Elem[cnt]-2*Elem[cnt+1]+Elem[cnt]) + R[cnt-1]*(Elem[cnt-1]-2*Elem[cnt]+Elem[cnt+1]))
+            else:
+                r5 += dx/2*(R[cnt+1]*Elem[cnt+1] + R[cnt]*Elem[cnt]) # O(h^2)
+                if cnt == 0: # first node on LHS
+                    r4 += 1/(2*dx)*( R[cnt+1]*(Elem[cnt+2]-2*Elem[cnt+1]+Elem[cnt]) + 0 ) # O(h^2)
+                    # r4 += 1/(2*dx)*( R[cnt+1]*(Elem[cnt+2]-2*Elem[cnt+1]+Elem[cnt]) + R[cnt]/4*(Elem[cnt]-2*Elem[cnt+1]+Elem[cnt+2]) ) # O(h)
+                elif cnt == len(R)-2: # next to last node on right side
+                    r4 += 1/(2*dx)*( 0 + R[cnt]*(Elem[cnt+1]-2*Elem[cnt]+Elem[cnt-1]) ) # O(h^2)
+                    # r4 += 1/(2*dx)*( R[cnt+1]*(Elem[cnt-1]-2*Elem[cnt]+Elem[cnt+1]) + R[cnt]*(Elem[cnt+1]-2*Elem[cnt]+Elem[cnt-1]) ) # O(h)
                 else:
-                    r4 += 1/4 * (R[cnt]*(R[cnt+1]-2*R[cnt]+R[cnt-1]) + R[cnt-1]*(R[cnt]-2*R[cnt-1]+R[cnt-2]))
-                r5 += dx/2 * (R[cnt]*Elem[cnt]+R[cnt-1]*Elem[cnt-1])
+                    r4 += 1/(2*dx)*( R[cnt+1]*(Elem[cnt+2]-2*Elem[cnt+1]+Elem[cnt]) + R[cnt]*(Elem[cnt+1]-2*Elem[cnt]+Elem[cnt-1]) ) # O(h^2)
     return(r1,r2,r3,r4,r5)
 
 def updateS(S,T,dt):
     s1=0.0; s2=0.0; s3=0.0; s4=0.0; s5=0.0
     for ids,dum in enumerate(S):
-        if ((ids==0) or (ids==len(S)-1)): # boundary points
+        if ids == len(S)-1:
             pass
-        else: # interior points
-            s1 += dt/2 * (math.pow(S[ids],2)+math.pow(S[ids-1],2))
-            s2 += 1/4 * (S[ids]*(S[ids+1]-S[ids]) + S[ids-1]*(S[ids]-S[ids-1]))
-            s3 += dt/2 * (S[ids]+S[ids-1])
+        else:
+            s1 += dt/2*( math.pow(S[ids+1],2) + math.pow(S[ids],2) )# O(h^2)
+            s3 += dt/2*( S[ids+1] + S[ids] )# O(h^2)
+            if ids == 0: # first node on left side
+                s2 += 1/2*( S[ids+1]*(S[ids+1]-S[ids]) + 0 ) # O(h^2)
+                # s2 += 1/2*( S[ids+1]*(S[ids+1]-S[ids]) + S[ids]*(S[ids+1]-S[ids]) ) # Forward Euler on second derivative,  O(h)
+            else:
+                s2 += 1/2*( S[ids+1]*(S[ids+1]-S[ids]) + S[ids]*(S[ids]-S[ids-1]) ) # Backward Euler on both derivatives, O(h)
     for Elem in T:
         for cnt,pair in enumerate(zip(Elem,S)):
-            if ((cnt==0) or (cnt==np.shape(T)[1]-1)): #boundary points
+            if cnt == len(S)-1:
                 pass
-            else: #interior points
-                s4 += 1/4 * (S[cnt]*(Elem[cnt+1]-Elem[cnt]) + S[cnt-1]*(Elem[cnt]-Elem[cnt-1]))
-                s5 += dt/2 * (S[cnt]*Elem[cnt] + S[cnt-1]*Elem[cnt-1])
+            else:
+                s5 += dt/2*( S[cnt+1]*Elem[cnt+1] + S[cnt]*Elem[cnt] ) # O(h^2)
+                if cnt == 0: # first node on left side
+                    s4 += 1/2*( S[cnt+1]*(Elem[cnt+1] - Elem[cnt]) + S[cnt]*(Elem[cnt+1] - Elem[cnt]) ) # Forward Euler on second derivative,  O(h)
+                else:
+                    s4 += 1/2*( S[cnt+1]*(Elem[cnt+1] - Elem[cnt]) + S[cnt]*(Elem[cnt] - Elem[cnt-1]) ) # Backward Euler on both derivatives, O(h)
     return(s1,s2,s3,s4,s5)
 
 def updateW(W,K,dk,k):
     w1=0.0; w2=0.0; w3=0.0; w4=0.0; w5=0.0
     for idw,dum in enumerate(W):
-        if ((idw==0) or (idw==np.shape(K)[1]-1)):
+        if idw == len(W)-1:
             pass
         else:
-            w1 += dk/2 * (math.pow(W[idw],2)+math.pow(W[idw-1],2))
-            w2 += k * dk/2 * (math.pow(W[idw],2)+math.pow(W[idw-1],2))
-            w3 += dk/2 * (W[idw]+W[idw-1])
+            w1 += dk/2*( math.pow(W[idw+1],2) + math.pow(W[idw],2) ) # O(h^2)
+            w2 += k[idw]*dk/2*( math.pow(W[idw+1],2) + math.pow(W[idw],2) ) # O(h^2)
+            w3 += dk/2*( W[idw+1] + W[idw] ) # O(h^2)
     for Elem in K:
         for cnt,pair in enumerate(zip(Elem,W)):
-            if ((cnt==0) or (cnt==np.shape(K)[1]-1)): #boundary points
+            if cnt == len(W)-1:
                 pass
             else:
-                w4 += dk/2 * (W[cnt]*Elem[cnt]+W[cnt-1]*Elem[cnt-1])
-                w5 += k * dk/2 * (W[cnt]*Elem[cnt]+W[cnt-2]*Elem[cnt-2])
+                w4 += dk/2*( W[cnt+1]*Elem[cnt+1] + W[cnt]*Elem[cnt] ) # O(h^2)
+                w5 += k[cnt]*dk/2*( W[cnt+1]*Elem[cnt+1] + W[cnt]*Elem[cnt] ) # O(h^2)
     return(w1,w2,w3,w4,w5)
+
+
+# def OLD_updateR(R,X,dx):
+#     r1=0; r2=0; r3=0; r4=0; r5=0
+#     for idr,dum in enumerate(R):
+#         if ((idr==0) or (idr==len(R)-1)): # boundary points
+#             pass
+#         else: # interior points
+#             r1 += dx/2 * (math.pow(R[idr],2)+math.pow(R[idr-1],2))
+#             if idr == 1:
+#                 r2 += math.pow(dx,3)/8 * (R[idr]*(R[idr]-2*R[idr+1]+R[idr]) + R[idr-1]*(R[idr-1]-2*R[idr]+R[idr+1]))
+#             else:
+#                 r2 += 1/4 * (R[idr]*(R[idr+1]-2*R[idr]+R[idr-1]) + R[idr-1]*(R[idr]-2*R[idr-1]+R[idr-2]))
+#             r3 += dx/2 * (R[idr]+R[idr-1])
+#     for Elem in X:
+#         for cnt,pair in enumerate(zip(Elem,R)):
+#             if ((cnt==0) or (cnt==np.shape(X)[1]-1)): #boundary points
+#                 pass
+#             else: #interior points
+#                 if cnt ==1:
+#                     r4 += math.pow(dx,3)/8 * (R[cnt]*(Elem[cnt]-2*Elem[cnt+1]+Elem[cnt]) + R[cnt-1]*(Elem[cnt-1]-2*Elem[cnt]+Elem[cnt+1]))
+#                 else:
+#                     r4 += 1/4 * (R[cnt]*(R[cnt+1]-2*R[cnt]+R[cnt-1]) + R[cnt-1]*(R[cnt]-2*R[cnt-1]+R[cnt-2]))
+#                 r5 += dx/2 * (R[cnt]*Elem[cnt]+R[cnt-1]*Elem[cnt-1])
+#     return(r1,r2,r3,r4,r5)
+# def OLD_updateS(S,T,dt):
+#     s1=0.0; s2=0.0; s3=0.0; s4=0.0; s5=0.0
+#     for ids,dum in enumerate(S):
+#         if ((ids==0) or (ids==len(S)-1)): # boundary points
+#             pass
+#         else: # interior points
+#             s1 += dt/2 * (math.pow(S[ids],2)+math.pow(S[ids-1],2))
+#             s2 += 1/4 * (S[ids]*(S[ids+1]-S[ids]) + S[ids-1]*(S[ids]-S[ids-1]))
+#             s3 += dt/2 * (S[ids]+S[ids-1])
+#     for Elem in T:
+#         for cnt,pair in enumerate(zip(Elem,S)):
+#             if ((cnt==0) or (cnt==np.shape(T)[1]-1)): #boundary points
+#                 pass
+#             else: #interior points
+#                 s4 += 1/4 * (S[cnt]*(Elem[cnt+1]-Elem[cnt]) + S[cnt-1]*(Elem[cnt]-Elem[cnt-1]))
+#                 s5 += dt/2 * (S[cnt]*Elem[cnt] + S[cnt-1]*Elem[cnt-1])
+#     return(s1,s2,s3,s4,s5)
+# def OLD_updateW(W,K,dk,k):
+#     w1=0.0; w2=0.0; w3=0.0; w4=0.0; w5=0.0
+#     for idw,dum in enumerate(W):
+#         if ((idw==0) or (idw==np.shape(K)[1]-1)):
+#             pass
+#         else:
+#             w1 += dk/2 * (math.pow(W[idw],2)+math.pow(W[idw-1],2))
+#             w2 += k * dk/2 * (math.pow(W[idw],2)+math.pow(W[idw-1],2))
+#             w3 += dk/2 * (W[idw]+W[idw-1])
+#     for Elem in K:
+#         for cnt,pair in enumerate(zip(Elem,W)):
+#             if ((cnt==0) or (cnt==np.shape(K)[1]-1)): #boundary points
+#                 pass
+#             else:
+#                 w4 += dk/2 * (W[cnt]*Elem[cnt]+W[cnt-1]*Elem[cnt-1])
+#                 w5 += k * dk/2 * (W[cnt]*Elem[cnt]+W[cnt-2]*Elem[cnt-2])
+#     return(w1,w2,w3,w4,w5)