a9349c02597c145b71ba984cd9f7ed28329f63bb
albeanth <albertia@oregonstate.edu>
2016-11-08 16:07:33 -0800

    seems to be working for iterations 1-3. However, iteration 4 is oscillatory for some odd reason....

--- 1DHeatEqnPGD.py
+++ 1DHeatEqnPGD.py
@@ -21,17 +21,21 @@ Xsize = 61
 Tsize = 151
 Ksize = 101
 
+Xrange = 1-0
+Trange = 0.1-0
+Krange = 5-1
+dx = (Xrange)/(Xsize-1)
+dt = (Trange)/(Tsize-1)
+dk = (Krange)/(Ksize-1)
+
 k = np.linspace(1,5,Ksize) # W/(m*K)
 f = 1 # constant source term
 
 ###############################################################################
 
-X = np.array([np.zeros(Xsize)],ndmin=2) # spatial domain -> calculated values for R for first iteration are independent of this first initial guess. tested with parabolic and linear inital shapes and same X_i+1 result was obtained.
-T = np.array([np.zeros(Tsize)],ndmin=2) # time domain
-K = np.array([np.zeros(Ksize)],ndmin=2) # diffusivity domain
-dx = (1-0)/Xsize
-dt = (0.1-0)/Tsize
-dk = (5-1)/Ksize
+X = np.array([np.ones(Xsize)],ndmin=2) # spatial domain -> calculated values for R for first iteration are independent of this first initial guess. tested with parabolic and linear inital shapes and same X_i+1 result was obtained.
+T = np.array([np.ones(Tsize)],ndmin=2) # time domain
+K = np.array([np.ones(Ksize)],ndmin=2) # diffusivity domain
 
 ## guesses for R,S,W
 R_old = np.ones(Xsize)
@@ -47,22 +51,34 @@ W_new = np.ones(Ksize)
 
 ## convergence criteria
 eps = 1E-4 # num of enrichment steps - 2-norm
-nu = 1E-5 # within enrichemnt - inf-norm
+nu = 1E-8 # within enrichemnt - inf-norm
 
 NumEnr=0 # number of enrichment steps
 eps_check = 1.0
 while eps_check > eps:
+    R_old = np.ones(Xsize)
+    R_old[0] = 0; R_old[-1] = 0
+
+    S_old = np.ones(Tsize)
+    S_old[0] = 0
+    S_new = np.ones(Tsize)
+    S_new[0] = 0
+
+    W_old = np.ones(Ksize)
+    W_new = np.ones(Ksize)
+
     enrCnt=0 # number of steps for an enrichment step to converge
     nu_check=1
+    s1,s2,s3,s4,s5 = basis.updateS(S_old,T,dt)
+    print('s integrals = ')
+    print(s1,s2,s3,s4,s5)
     while nu_check >= nu:
         #######################
         ##   Solve for R(x)  ##
         #######################
-        # print('\nSolving for R(x)')
-        s1,s2,s3,s4,s5 = basis.updateS(S_old,T,dt)
-        # print('updatedS -> '+str(s1)+', '+str(s2)+', '+str(s3)+', '+str(s4)+', '+str(s5))
         w1,w2,w3,w4,w5 = basis.updateW(W_old,K,dk,k)
-        # print('updatedW -> '+str(w1)+', '+str(w2)+', '+str(w3)+', '+str(w4)+', '+str(w5))
+        print('w integrals = ')
+        print(w1,w2,w3,w4,w5)
         v1 = -(s1*w2/(math.pow(dx,2)))
         v2 = s2*w1 + (2*s1*w2)/math.pow(dx,2)
         v3 = v1
@@ -83,143 +99,182 @@ while eps_check > eps:
         R_Lmatrix[-1,-1] = 1.0
 
         R_new = np.dot(np.linalg.inv(R_Lmatrix),b)
-        # R_new = linalg.solve_triangular(R_Lmatrix,bVec)
+        # R_new = np.linalg.solve(R_Lmatrix,b)
 
         #######################
         ##   Solve for S(t)  ##
         #######################
-        # print('Solving for S(t)')
         r1,r2,r3,r4,r5 = basis.updateR(R_new,X,dx)
-        # print('updatedR -> '+str(r1)+', '+str(r2)+', '+str(r3)+', '+str(r4)+', '+str(r5))
+        print('r integrals = ')
+        print(r1,r2,r3,r4,r5)
         # All 'w' integrals already solved for in R(x) solve. Don't need to resolve them.
-
-        # for idy in np.arange(0,Tsize):
-        #     if idy == 0:
-        #         S_new[idy] = 0.0
-        #     else:
-        #         tmp = 0.0
-        #         for ide,Elem in enumerate(T):
-        #             tmp += r5[ide]*w4[ide]*(Elem[idy]-Elem[idy-1])/dt - Elem[idy]*w5[ide]*r4[ide]
-        #         S_new[idy] = (S_new[idy-1]*w1*r1/dt - tmp + w3*r3*f)/(w1*r1/dt - w2*r2)
-        # print(S_new)
 
         for idy in np.arange(0,Tsize-1):
             tmp = 0.0
             for ide,Elem in enumerate(T):
-                tmp += r5[ide]*w4[ide]*(Elem[idy+1]-Elem[idy])/dt - Elem[idy]*w5[ide]*r4[ide]
+                tmp += r5[ide]*w4[ide]*(Elem[idy+1]-Elem[idy])/dt - Elem[idy+1]*w5[ide]*r4[ide]
             S_new[idy+1] = (S_new[idy]*w1*r1/dt - tmp + w3*r3*f)/(w1*r1/dt - w2*r2)
 
         #######################
         ##   Solve for W(k)  ##
         #######################
-        # print('Solving for W(k)')
         # All 'r' integrals already solved for in R(x) solve. Don't need to resolve them.
         s1,s2,s3,s4,s5 = basis.updateS(S_new,T,dt)
-        # print('updatedS -> '+str(s1)+', '+str(s2)+', '+str(s3)+', '+str(s4)+', '+str(s5))
-
+        print('s integrals = ')
+        print(s1,s2,s3,s4,s5)
         for idk,param in enumerate(k): # each element in conductivity range
             tmp = 0.0
             for ide,Elem in enumerate(K): # each enrichment step
-                # print(Elem[idk],r5[ide],s4[ide],r4[ide],s5[ide])
                 tmp += Elem[idk]*r5[ide]*s4[ide] - param*Elem[idk]*r4[ide]*s5[ide]
-            # print(tmp, (f*r3*s3), r1*s2-param*s1*r2)
             W_new[idk] = (-tmp + f*r3*s3)/(r1*s2-param*s1*r2)
-        # print(W_new)
+
         #######################################
         ## Check for enrichment convergence  ##
         #######################################
 
-        nu_check = 0.0
-        for idr in np.arange(0,Xsize):
-            for ids in np.arange(0,Tsize):
-                for idw in np.arange(0,Ksize):
-                    tmp = abs((R_new[idr]*S_new[ids]*W_new[idw] - R_old[idr]*S_old[ids]*W_old[idw]))
-                    if tmp > nu_check:
-                        nu_check = tmp
-                    else:
-                        pass
+        ## actual convergence
+        intR_new = 0.0; intR_old = 0.0; intR_both = 0.0
+        for idr in np.arange(0,Xsize-1):
+            intR_new += dx/2*(R_new[idr+1]**2 + R_new[idr]**2)
+            intR_old += dx/2*(R_old[idr+1]**2 + R_old[idr]**2)
+            intR_both += dx/2*(R_new[idr+1]*R_old[idr+1]+R_new[idr]*R_old[idr])
+        # print('convergence params \n',str(intR_new), str(intR_old), str(intR_both))
+        # sys.exit()
+
+        intS_new=0.0; intS_old=0.0; intS_both = 0.0
+        for ids in np.arange(0,Tsize-1):
+            intS_new += dt/2*(S_new[ids+1]**2 + S_new[ids]**2)
+            intS_old += dt/2*(S_old[ids+1]**2 + S_old[ids]**2)
+            intS_both += dt/2*(S_new[ids+1]*S_old[ids+1]+S_new[ids]*S_old[ids])
+        # print(intS_new, intS_old, intS_both)
+        # sys.exit()
+
+        intW_new=0.0; intW_old=0.0; intW_both = 0.0
+        for idw in np.arange(0,Ksize-1):
+            intW_new += dk/2*(W_new[idw+1]**2 + W_new[idw]**2)
+            intW_old += dk/2*(W_old[idw+1]**2 + W_old[idw]**2)
+            intW_both += dk/2*(W_new[idw+1]*W_old[idw+1]+W_new[idw]*W_old[idw])
+        # print(intW_new, intW_old, intW_both)
+        # sys.exit()
+
+        # print((intR_new*intS_new*intW_new) + (intR_old*intS_old*intW_old))
+        # print(2*(intR_both*intS_both*intW_both))
+        # sys.exit()
+        nu_check = math.sqrt( (intR_new*intS_new*intW_new) + (intR_old*intS_old*intW_old) - 2*(intR_both*intS_both*intW_both) )
+
+        ## palmer's interpretation
+        # nu_check = 0.0
+        # for idr in np.arange(0,Xsize):
+        #     for ids in np.arange(0,Tsize):
+        #         for idw in np.arange(0,Ksize):
+        #             tmp = abs(R_new[idr]*S_new[ids]*W_new[idw] - R_old[idr]*S_old[ids]*W_old[idw])
+        #             if tmp > nu_check:
+        #                 nu_check = tmp
+        #             else:
+        #                 pass
+        #
+        ## tony's interpretation
+        # Num = 0.0; Denom = 0.0
+        # for xNew,xOld in zip(R_new,R_old):
+        #     for tNew,tOld in zip(S_new,S_old):
+        #         for kNew,kOld in zip(W_new,W_old):
+        #             new = xNew*tNew*kNew
+        #             old = xOld*tOld*kOld
+        #             Num += abs(new - old)
+        #             # Denom += abs(old)
+        # # nu_check = Num/Denom
+        # nu_check = Num
 
         print(Cyan+str(nu_check))
         enrCnt += 1
-        # sys.exit()
         if nu_check >= nu:
             R_old = R_new
             S_old = S_new
             W_old = W_new
-            # if enrCnt == 1:
-            #     print(Red+'maximum iteration count reached.')
-            #     X = np.vstack((X,R_new))
-            #     T = np.vstack((T,S_new))
-            #     K = np.vstack((K,W_new))
-            #     break
         else:
             NumEnr += 1 # counter for number of steps for enrichment step convergence
             print(Green+'Enrichment Step '+str(NumEnr)+' completed in '+str(enrCnt)+' steps.')
-            X = np.vstack((X,R_new))
-            T = np.vstack((T,S_new))
-            K = np.vstack((K,W_new))
-
+            R_new_Norm = math.sqrt(intR_new/(Xrange**2))
+            S_new_Norm = math.sqrt(intS_new/(Trange**2))
+            W_new_Norm = math.sqrt(intW_new/(Krange**2))
+            RSW_new = math.pow((R_new_Norm*S_new_Norm*W_new_Norm),1/3)
+            print(R_new_Norm,S_new_Norm,W_new_Norm,RSW_new)
+            X = np.vstack((X,RSW_new*R_new/R_new_Norm))
+            T = np.vstack((T,RSW_new*S_new/S_new_Norm))
+            K = np.vstack((K,RSW_new*W_new/W_new_Norm))
     print(Yellow+'   ....checking enrichment convergence')
 
-    TotSum = 0.0; NewSum = 0.0
-    for enr in np.arange(0,len(X)):
-        for idr in np.arange(0,Xsize):
-            for idt in np.arange(0,Tsize):
-                for idk in np.arange(0,Ksize):
-                    TotSum += X[enr][idr]*T[enr][idt]*K[enr][idk]
-                    if enr == len(X)-1:
-                        NewSum += X[enr][idr]*T[enr][idt]*K[enr][idk]
+    ## actual convergence
+    R1 = 0.0; S1 = 0.0; W1 = 0.0
+    for idr in np.arange(0,Xsize-1):
+        R1 += dx/2*(X[1][idr+1]**2 + X[1][idr]**2)
+    for ids in np.arange(0,Tsize-1):
+        S1 += dt/2*(T[1][ids+1]**2 + T[1][ids]**2)
+    for idw in np.arange(0,Ksize-1):
+        W1 += dk/2*(K[1][idw+1]**2 + K[1][idw]**2)
+    eps_check = math.sqrt(intR_new*intS_new*intW_new)/math.sqrt(R1*S1*W1)
 
-    eps_check = abs(NewSum/TotSum)
+    ## tony's interpretation
+    # TotSum = 0.0; NewSum = 0.0
+    # for enr in np.arange(0,len(X)):
+    #     for idr in np.arange(0,Xsize):
+    #         for idt in np.arange(0,Tsize):
+    #             for idk in np.arange(0,Ksize):
+    #                 TotSum += abs(X[enr][idr]*T[enr][idt]*K[enr][idk])
+    #                 if enr == len(X)-1:
+    #                     NewSum += abs(X[enr][idr]*T[enr][idt]*K[enr][idk])
+    # eps_check = NewSum/TotSum
     print(Magenta+'   '+str(eps_check))
-    # sys.exit()
     if eps_check >= eps:
         if NumEnr == 3:
             break
-        R_old = X[-1]
-        S_old = T[-1]
-        W_old = K[-1]
+        # R_old = X[-1]
+        # S_old = T[-1]
+        # W_old = K[-1]
     else:
         print(Green+'Done. It took '+str(NumEnr)+ ' enrichement steps to converge solution.\n')
         break
 
 ###############################################################################
+print(X)
+print(T)
+print(K)
+sys.exit()
 
-def maxim(a):
-    maxx = 0
-    for elem in a:
-        if abs(elem)>maxx:
-            maxx = abs(elem)
-    return(maxx)
+def norm(Vec,size,stp):
+    tmp = 0.0
+    for idx in np.arange(0,size-1):
+        tmp += stp/2*(Vec[idx+1]**2 + Vec[idx]**2)
+    return(tmp)
 
 print('plots!')
+
+
 plt.figure(1)
-for cnt,elem in enumerate(X):
-    if cnt == 0:
-        pass
-    else:
-        plt.plot(np.linspace(0,1,Xsize), -elem/np.linalg.norm(elem,2), label = str(cnt), linewidth = 3)
+for cnt,elem in enumerate(X[1:]):
+    plt.plot(np.linspace(0,1,Xsize), elem/np.linalg.norm(elem,2), label = str(cnt), linewidth = 3) #elem/norm(elem,Xsize,dx)
 plt.grid(True)
 plt.legend(loc='best',fontsize='x-small')
 
 plt.figure(2)
-for cnt,elem in enumerate(T):
-    if cnt == 0:
-        pass
-    else:
-        plt.plot(np.linspace(0,0.1,Tsize), elem/np.linalg.norm(elem,2), label = str(cnt), linewidth = 3)
+for cnt,elem in enumerate(T[1:]):
+    plt.plot(np.linspace(0,0.1,Tsize), elem/np.linalg.norm(elem,2), label = str(cnt), linewidth = 3) #elem/norm(elem,Tsize,dt)
 plt.grid(True)
 plt.legend(loc='best',fontsize='x-small')
 
 plt.figure(3)
-for cnt,elem in enumerate(K):
-    if cnt == 0:
-        pass
-    else:
-        plt.plot(np.linspace(1,5,Ksize), -elem/np.linalg.norm(elem,2), label = str(cnt), linewidth = 3)
+for cnt,elem in enumerate(K[1:]):
+    plt.plot(np.linspace(1,5,Ksize), elem/np.linalg.norm(elem,2), label = str(cnt), linewidth = 3) #elem/norm(elem,Ksize,dk)
 plt.grid(True)
 plt.legend(loc='best',fontsize='x-small')
+
 plt.show()
+
+# fileout = open('dum.txt','w')
+# fileout.write(str(X)+'\n\n')
+# fileout.write(str(T)+'\n\n')
+# fileout.write(str(K)+'\n\n')
+# fileout.close()
 
 sys.exit()
 
--- basis.py
+++ basis.py
@@ -13,15 +13,16 @@ def updateR(R,X,dx):
             r2 += (1/(2*dx))*( 0 + R[idr]*(R[idr+1]-2*R[idr]+R[idr-1]) ) # O(h^2)
         else:
             r2 += (1/(2*dx))*( R[idr+1]*(R[idr+2]-2*R[idr+1]+R[idr]) + R[idr]*(R[idr+1]-2*R[idr]+R[idr-1]) ) # O(h^2)
-    for ide,Elem in enumerate(X):
-        for cnt in np.arange(0,len(R)-1):
-            r5[ide] += dx/2*(R[cnt+1]*Elem[cnt+1] + R[cnt]*Elem[cnt]) # O(h^2)
-            if cnt == 0: # first node on LHS
-                r4[ide] += (1/(2*dx))*( R[cnt+1]*(Elem[cnt+2]-2*Elem[cnt+1]+Elem[cnt]) + 0 ) # O(h^2)
-            elif cnt == len(R)-2: # next to last node on right side
-                r4[ide] += (1/(2*dx))*( 0 + R[cnt]*(Elem[cnt+1]-2*Elem[cnt]+Elem[cnt-1]) ) # O(h^2)
-            else:
-                r4[ide] += (1/(2*dx))*( R[cnt+1]*(Elem[cnt+2]-2*Elem[cnt+1]+Elem[cnt]) + R[cnt]*(Elem[cnt+1]-2*Elem[cnt]+Elem[cnt-1]) ) # O(h^2)
+    if len(X)>1:
+        for ide,Elem in enumerate(X):
+            for cnt in np.arange(0,len(R)-1):
+                r5[ide] += dx/2*(R[cnt+1]*Elem[cnt+1] + R[cnt]*Elem[cnt]) # O(h^2)
+                if cnt == 0: # first node on LHS
+                    r4[ide] += (1/(2*dx))*( R[cnt+1]*(Elem[cnt+2]-2*Elem[cnt+1]+Elem[cnt]) + 0 ) # O(h^2)
+                elif cnt == len(R)-2: # next to last node on right side
+                    r4[ide] += (1/(2*dx))*( 0 + R[cnt]*(Elem[cnt+1]-2*Elem[cnt]+Elem[cnt-1]) ) # O(h^2)
+                else:
+                    r4[ide] += (1/(2*dx))*( R[cnt+1]*(Elem[cnt+2]-2*Elem[cnt+1]+Elem[cnt]) + R[cnt]*(Elem[cnt+1]-2*Elem[cnt]+Elem[cnt-1]) ) # O(h^2)
     return(r1,r2,r3,r4,r5)
 
 def updateS(S,T,dt):
@@ -33,13 +34,14 @@ def updateS(S,T,dt):
             s2 += 1/2*( S[ids+1]*(S[ids+1]-S[ids]) + 0 ) # O(h^2)
         else:
             s2 += 1/2*( S[ids+1]*(S[ids+1]-S[ids]) + S[ids]*(S[ids]-S[ids-1]) ) # Backward Euler on both derivatives, O(h)
-    for ide,Elem in enumerate(T):
-        for cnt in np.arange(0,len(S)-1):
-            s5[ide] += dt/2*( S[cnt+1]*Elem[cnt+1] + S[cnt]*Elem[cnt] ) # O(h^2)
-            if cnt == 0: # first node on left side
-                s4[ide] += 1/2*( S[cnt+1]*(Elem[cnt+1]-Elem[cnt]) + 0 ) # O(h^2)
-            else:
-                s4[ide] += 1/2*( S[cnt+1]*(Elem[cnt+1]-Elem[cnt]) + S[cnt]*(Elem[cnt]-Elem[cnt-1]) ) # Backward Euler on both derivatives, O(h)
+    if len(T)>1:
+        for ide,Elem in enumerate(T):
+            for cnt in np.arange(0,len(S)-1):
+                s5[ide] += dt/2*( S[cnt+1]*Elem[cnt+1] + S[cnt]*Elem[cnt] ) # O(h^2)
+                if cnt == 0: # first node on left side
+                    s4[ide] += 1/2*( S[cnt+1]*(Elem[cnt+1]-Elem[cnt]) + 0 ) # O(h^2)
+                else:
+                    s4[ide] += 1/2*( S[cnt+1]*(Elem[cnt+1]-Elem[cnt]) + S[cnt]*(Elem[cnt]-Elem[cnt-1]) ) # Backward Euler on both derivatives, O(h)
     return(s1,s2,s3,s4,s5)
 
 def updateW(W,K,dk,k):
@@ -48,8 +50,9 @@ def updateW(W,K,dk,k):
         w1 += dk/2*( math.pow(W[idw+1],2) + math.pow(W[idw],2) ) # O(h^2)
         w2 += dk/2*( k[idw+1]*math.pow(W[idw+1],2) + k[idw]*math.pow(W[idw],2) ) # O(h^2)
         w3 += dk/2*( W[idw+1] + W[idw] ) # O(h^2)
-    for ide,Elem in enumerate(K):
-        for cnt in np.arange(0,len(W)-1):
-            w4[ide] += dk/2*( W[cnt+1]*Elem[cnt+1] + W[cnt]*Elem[cnt] ) # O(h^2)
-            w5[ide] += dk/2*( k[cnt+1]*W[cnt+1]*Elem[cnt+1] + k[cnt]*W[cnt]*Elem[cnt] ) # O(h^2)
+    if len(K)>1:
+        for ide,Elem in enumerate(K):
+            for cnt in np.arange(0,len(W)-1):
+                w4[ide] += dk/2*( W[cnt+1]*Elem[cnt+1] + W[cnt]*Elem[cnt] ) # O(h^2)
+                w5[ide] += dk/2*( k[cnt+1]*W[cnt+1]*Elem[cnt+1] + k[cnt]*W[cnt]*Elem[cnt] ) # O(h^2)
     return(w1,w2,w3,w4,w5)
