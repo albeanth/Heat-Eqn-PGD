90a64b89cafc5b392b65908384af562f6a844830
albeanth <albertia@oregonstate.edu>
2017-12-18 11:59:54 -0800

    pushing up plotter to bring repo up to date with latest revisions. going to remove repo from local cpu. will be losing 4 outputted data files and two outputted figures. No worries though. In theory you should be able to just rerun the source code and get this data - or something darn similar to it - right back.

--- plotters/BasisFuncPlot.sh
+++ plotters/BasisFuncPlot.sh
@@ -28,7 +28,7 @@ reset
 set grid ytics mytics
 set title "Conductivity Basis Functions"
 set key at 4.5,1.4 font ",13"
-set term postscript eps enhanced color blacktext "Helvetica" 18
+set term postscript eps enhanced color blacktext "Helvetica" 18 background rgb 'white'
 set output "BasisFunc_Conductivity.eps"
 plot  "../data/Raw_BasisFunc.txt" using 1:2 with linespoints lt -1 lc 'black' lw 3  ps 1.5 title '1',\
       "../data/Raw_BasisFunc.txt" using 1:3 with linespoints lt -1 lc 'blue' lw 3  ps 1.5 title '2',\
